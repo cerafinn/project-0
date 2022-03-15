08be457ae75212c40bd1e5754fd3cde5898b3609
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-15 00:39:45 -0400

    working on controller for clients, accounts

--- src/main/java/com/revature/controller/AccountController.java
+++ src/main/java/com/revature/controller/AccountController.java
@@ -1,5 +1,25 @@
 package com.revature.controller;
 
-public class AccountController {
-  //create routes for queries within dao
+import com.revature.model.Account;
+import com.revature.service.AccountService;
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+
+import java.util.List;
+
+public class AccountController implements Controller {
+  //create routes for accounts from queries
+  public AccountService accountService;
+
+  public AccountController() { this.accountService = new AccountService(); }
+
+  private Handler getAllAccounts = (ctx) -> {
+    List<Account> accounts = accountService.getAllAccounts();
+    ctx.json(accounts);
+  };
+
+  @Override
+  public void mapEndpoints(Javalin app) {
+    app.get("/clients/{clientId}/accounts", getAllAccounts);
+  }
 }
--- src/main/java/com/revature/controller/ClientController.java
+++ src/main/java/com/revature/controller/ClientController.java
@@ -1,5 +1,25 @@
 package com.revature.controller;
 
-public class ClientController {
-  //create routes from queries within dao
+import com.revature.model.Client;
+import com.revature.service.ClientService;
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+
+import java.util.List;
+
+public class ClientController implements Controller {
+  //create routes for clients from queries
+  public ClientService clientService;
+
+  public ClientController() { this.clientService = new ClientService(); }
+
+  private Handler getAllClients = (ctx) -> {
+    List<Client> clients = clientService.getAllClients();
+    ctx.json(clients);
+  };
+
+  @Override
+  public void mapEndpoints(Javalin app) {
+    app.get("/clients", getAllClients);
+  }
 }
