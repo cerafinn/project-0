25e40611f975e96b7d9ea02dc2cfa69042683af5
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-03-15 04:10:56 -0400

    added exception controller and driver method

--- src/main/java/com/revature/controller/ExceptionController.java
+++ src/main/java/com/revature/controller/ExceptionController.java
@@ -1,8 +1,42 @@
 package com.revature.controller;
 
-public class ExceptionController {
+import com.revature.exception.AccountNotFoundException;
+import com.revature.exception.ClientNotFoundException;
+import io.javalin.Javalin;
+import io.javalin.http.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExceptionController implements Controller {
   //create exceptions for:
   //client not found
   //invalid entries
   //account not found
+
+  private Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+  private ExceptionHandler clientNotFound = (e, ctx) -> {
+    logger.warn("Client attempted to retrieve does not exist: " + e.getMessage());
+    ctx.status(404);
+    ctx.json(e.getMessage());
+  };
+
+  private ExceptionHandler accountNotFound = (e, ctx) -> {
+    logger.warn("Account attempted to retrieve does not exist: " + e.getMessage());
+    ctx.status(404);
+    ctx.json(e.getMessage());
+  };
+
+  private ExceptionHandler badArgument = (e, ctx) -> {
+    logger.warn("User input a bad argument. Exception message is " + e.getMessage());
+    ctx.status(400);
+    ctx.json(e.getMessage());
+  };
+
+  @Override
+  public void mapEndpoints(Javalin app) {
+    app.exception(ClientNotFoundException.class, clientNotFound);
+    app.exception(AccountNotFoundException.class, accountNotFound);
+    app.exception(IllegalArgumentException.class, badArgument);
+  }
 }
--- src/main/java/com/revature/main/Driver.java
+++ src/main/java/com/revature/main/Driver.java
@@ -1,5 +1,28 @@
 package com.revature.main;
 
+import com.revature.controller.AccountController;
+import com.revature.controller.ClientController;
+import com.revature.controller.Controller;
+import com.revature.controller.ExceptionController;
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Driver {
   //create server methods for controller endpoints
+  public static Logger logger = LoggerFactory.getLogger(Driver.class);
+
+  public static void main(String[] args) {
+    Javalin app = Javalin.create();
+
+    app.before((ctx) -> { logger.info(ctx.method() + " request received for " + ctx.path()); });
+    mapControllers(app, new ClientController(), new AccountController(), new ExceptionController());
+    app.start();
+  }
+
+  public static void mapControllers(Javalin app, Controller... controllers) {
+    for (Controller c : controllers) {
+      c.mapEndpoints(app);
+    }
+  }
 }
